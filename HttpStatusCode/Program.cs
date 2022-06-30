806e703f1be7275320acf31d3aa86a4d93784406
Muhammet ÇETİN <muhammedcetin94@gmail.com>
2022-06-30 11:57:53 +0300

    webapidb

--- HttpStatusCode/Program.cs
+++ HttpStatusCode/Program.cs
@@ -1,3 +1,6 @@
+using HttpStatusCode.Infrastructure.Contex;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<SqlDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("WebApiDb"));
+});
 
 var app = builder.Build();
 
