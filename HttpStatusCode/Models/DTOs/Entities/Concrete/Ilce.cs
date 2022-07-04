1822f694eaf141b6ccc0903d8c631b0a4bbc3156
Muhammet ÇETİN <muhammedcetin94@gmail.com>
2022-07-04 10:18:45 +0300

    personel mvc projesi oluşturuldu

---
+++ HttpStatusCode/Models/DTOs/Entities/Concrete/Ilce.cs
@@ -0,0 +1,12 @@
+﻿using HttpStatusCode.Models.DTOs.Entities.Abstract;
+
+namespace HttpStatusCode.Models.DTOs.Entities.Concrete
+{
+    public class Ilce : BaseEntity
+    {
+        public string IlceAdi { get; set; }
+        public int SehirId { get; set; }
+        public Sehir Sehir { get; set; }
+
+    }
+}
---
+++ HttpStatusCode/Models/DTOs/Entities/Concrete/Sehir.cs
@@ -0,0 +1,10 @@
+﻿using HttpStatusCode.Models.DTOs.Entities.Abstract;
+
+namespace HttpStatusCode.Models.DTOs.Entities.Concrete
+{
+    public class Sehir : BaseEntity
+    {
+        public string SehirAdi { get; set; }
+        public ICollection<Ilce> Ilce { get; set; }
+    }
+}
