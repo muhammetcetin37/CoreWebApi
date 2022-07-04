2cf301d268d6d871bda30719dfa630266ba74ce1
Muhammet ÇETİN <muhammedcetin94@gmail.com>
2022-07-04 11:59:14 +0300

    sehir ve ilce için repository ler eklendi

---
+++ HttpStatusCode/Controllers/SehirController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HttpStatusCode.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SehirController : ControllerBase
+    {
+    }
+}
---
+++ HttpStatusCode/Infrastructure/Repository/Concrete/IlceDAL.cs
@@ -0,0 +1,9 @@
+﻿using HttpStatusCode.Infrastructure.Repository.Abstract;
+using HttpStatusCode.Models.DTOs.Entities.Concrete;
+
+namespace HttpStatusCode.Infrastructure.Repository.Concrete
+{
+    public class IlceDAL : RepositoryBase<Ilce>, IilceDAL
+    {
+    }
+}
