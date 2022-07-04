30031d0260681ef5efc4a2b7c8d573794040dfd9
Muhammet ÇETİN <muhammedcetin94@gmail.com>
2022-07-04 10:36:17 +0300

    entity yazıldı

---
+++ CoreMvcPersonel/Models/Entities/Adres.cs
@@ -0,0 +1,21 @@
+﻿namespace CoreMvcPersonel.Models.Entities
+{
+    public enum AdresTip
+    {
+        Ev,
+        Is,
+        Yazlik,
+        Diger
+    }
+
+    public class Adres
+    {
+
+        public int Id { get; set; }
+        public AdresTip? AdresTip { get; set; }
+        public int? SehirId { get; set; }
+        public int? IlceId { get; set; }
+        public string? CaddeSokak { get; set; }
+
+    }
+}
